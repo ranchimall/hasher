0aa7ab54e734f9317e204c15890ee1b4794d8132
sairaj mote <sairajmote3@gmail.com>
2024-01-16 04:35:37 +0530

    testing  bug fix

--- routes/is-tor.js
+++ routes/is-tor.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-
 router.get('/', async (req, res) => {
     try {
-        const ip = req.socket.remoteAddress;
-        const response = await axios.get(`https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=${ip}`);
-        const isTor = response.data.includes(ip);
-        res.json({ isTor, ip });
+        console.log(req.ip, req.socket.remoteAddress, req.connection.remoteAddress, req.headers['x-forwarded-for'])
+        const ip = req.ip;
+        const response = await axios.get(`https://check.torproject.org/api/ip?ip=${ip}`);
+        res.json({
+            ...response.data,
+            reqIp: ip,
+            reqSocketRemoteAddress: req.socket.remoteAddress,
+            reqConnectionRemoteAddress: req.connection.remoteAddress,
+            reqHeadersXForwardedFor: req.headers['x-forwarded-for']
+        });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
